15c9aaf997dba9b8875c4542dcee0a672f89a88a
Sinii Viacheslav <siniy.vyacheslav@gmail.com>
2022-11-30 18:04:35 +0300

    basic game loop and tournament

--- snowball_fight/playground/game_loop.py
+++ snowball_fight/playground/game_loop.py
@@ -1,4 +1,19 @@
+from random import randint
+
+import numpy as np
+import multiprocessing as mp
+
+
 def game_loop(player1, player2, initial_num_balls=100, total_num_steps=60):
+    """
+    Take two players as input and perform a game.
+
+    :param player1: player1
+    :param player2: player2
+    :param initial_num_balls: number of balls for each player in the start of the game
+    :param total_num_steps: number of steps in the game
+    :return: payoffs of players in the end of the game
+    """
     player1_num_balls = initial_num_balls
     player2_num_balls = initial_num_balls
 
@@ -61,5 +76,90 @@ def game_loop(player1, player2, initial_num_balls=100, total_num_steps=60):
 
     return player1_num_balls, player2_num_balls
 
+
+# Taken from https://www.geeksforgeeks.org/random-list-of-m-non-negative-integers-whose-sum-is-n/
+def random_list_sums_to(list_size, list_sum):
+    """
+    Create a random list such that all elements sum to 'list_sum'.
+
+    :param list_size: size of the list
+    :param list_sum: elements of list should sum up to this number
+    :return: random list
+    """
+    # Create an array of size m where
+    # every element is initialized to 0
+    arr = [0] * list_size
+
+    # To make the sum of the final list as n
+    for i in range(list_sum):
+        # Increment any random element
+        # from the array by 1
+        arr[randint(0, list_sum) % list_size] += 1
+
+    return arr
+
+
+def replicate_each_player_n_times(player_types, player_nums):
+    """
+    Returns a list of players where 'player_types[i]' is replicated 'player_nums[i]' times.
+
+    :param player_types: types of players
+    :param player_nums: number of occurences of each player
+    :return: list of players
+    """
+    players = []
+    for player_num, player_type in zip(player_nums, player_types):
+        players.extend([player_type() for _ in range(player_num)])
+
+    return players
+
+
+def player_pairs(players):
+    """
+    Return all possible pairs of players.
+
+    :param players: list of players in all game.
+    :return: (player1, player2)
+    """
+
+    for i in range(len(players)):
+        for j in range(i + 1, len(players)):
+            yield players[i], players[j]
+
+
+def all_players_play(players, **kwargs):
+    """
+    Performs a play of the game between each pair of players.
+
+    :param players: list of players
+    :param kwargs: any parameters that should be passed to 'game_loop'
+    :return: payoffs of players after each game
+    """
+    with mp.Pool(-1) as pool:
+        results = pool.map(lambda x: game_loop(x[0], x[1], **kwargs), player_pairs(players))
+
+    results = np.array(results).reshape((len(players), len(players), 2))
+
+    return results
+
+
+def generate_population(player_types: list, population_size=50):
+    """
+    Generate population of size 'population_size' with types of players 'player_types'.
+    :param player_types:
+    :param population_size:
+    :return:
+    """
+
+    num_players = random_list_sums_to(len(player_types), population_size)
+    players = replicate_each_player_n_times(player_types, num_players)
+
+    return players
+
+
 if __name__ == '__main__':
-    pass
+    player_types = []
+    players = generate_population(player_types)
+    results = all_players_play(players)
+
+    print(results)
