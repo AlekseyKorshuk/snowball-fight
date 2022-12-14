3ffb1aba9474780d4818f3dd184077d6ed738454
Sinii Viacheslav <siniy.vyacheslav@gmail.com>
2022-12-15 00:27:00 +0300

    dash app

--- snowball_fight/agents/all_c.py
+++ snowball_fight/agents/all_c.py
@@ -12,6 +12,7 @@ class AllCAgent(BaseAgent):
     @BaseAgent.on_step
     def shoot_to_hot_field(self, opponent_last_shot_to_your_field, snowball_number,
                            minutes_passed_after_your_shot) -> int:
+        print(self.current_step, self.total_steps)
         if (minutes_passed_after_your_shot % 4 == 0 and minutes_passed_after_your_shot != 0) or self.current_step == self.total_steps:
             return min(
                 snowball_number,
