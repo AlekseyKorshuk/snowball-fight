6165a580e8ed237bd4d90e8c5d1775f655bcbb24
Aleksey Korshuk <ale-kor02@mail.ru>
2022-12-14 21:10:29 +0300

    updates

--- snowball_fight/dash_app.py
+++ snowball_fight/dash_app.py
@@ -1,11 +1,11 @@
-
 from dash import Dash
-from dash_components import payoff_component
+from dash_components import payoff_component, get_layout
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = payoff_component()
+app.layout = get_layout()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
